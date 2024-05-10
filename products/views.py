7d267a30d520a98584e8dce14dc86ce62ed9ad2f
XOZAN Aykut <hozanbesir21@gmail.com>
2024-05-10 13:20:26 +0000

    Updated delivery banner for better visibility and design

--- products/views.py
+++ products/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
 from .models import Product
 
+# Create your views here.
+
 def all_products(request):
-    """A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
-    query = None
-    categories = None
-    sort = None
-    direction = None
+
     context = {
         'products': products,
-        'query': query,
-        'categories': categories,
-        'sort': sort,
-        'direction': direction,
     }
+
     return render(request, 'products/products.html', context)
