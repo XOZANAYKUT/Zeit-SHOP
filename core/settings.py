a568b03893337e45d79b63c20c6ac8f319a473b7
XOZAN Aykut <hozanbesir21@gmail.com>
2024-05-31 09:58:45 +0000

    updated settings.py

--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG =  os.environ.get("SECRET_KEY")
-
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'zeitshoop1-c40db427d283.herokuapp.com',
