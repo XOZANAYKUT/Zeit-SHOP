18d4fe848571634325c7819758a95c28cb125a8d
XOZAN Aykut <hozanbesir21@gmail.com>
2024-06-01 09:23:04 +0000

    update general style and Add responsive for general styles

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'zeitshoop1-c40db427d283.herokuapp.com',
