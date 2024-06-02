a1316ecec37df8e4c15a0a10562134d027376878
XOZAN Aykut <hozanbesir21@gmail.com>
2024-06-02 08:28:45 +0000

    update views.py

--- core/urls.py
+++ core/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'core.views.handler404'
